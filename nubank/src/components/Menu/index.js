af207bd1fc3653c6d1e8e49257182c5c0dfb1c56
Jean <jeanlucafp@gmail.com>
2019-05-04 12:01:08 -0300

    finalizado UI
    ...

--- nubank/src/components/Menu/index.js
+++ nubank/src/components/Menu/index.js
@@ -1,8 +1,48 @@
 import React from 'react'
-import { Container } from './styles';
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles';
+import QRcode from 'react-native-qrcode'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-export default function Menu(){
+export default function Menu({ translateY }){
     return(
-        <Container></Container>
+        <Container  style={{
+            opacity: translateY.interpolate({
+              inputRange: [0, 150],
+              outputRange: [0, 1]
+            })
+          }}>
+            <Code>
+                <QRcode value="#" size={80} fgColor="#FFF" bgColor="#8B10AE"></QRcode>
+            </Code>
+
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#FFF"></Icon>
+                    <NavText>Me ajuda</NavText>
+                </NavItem>
+
+
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#FFF"></Icon>
+                    <NavText>Perfil</NavText>
+                </NavItem>
+
+
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#FFF"></Icon>
+                    <NavText>Configurar Cartao</NavText>
+                </NavItem>
+
+
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#FFF"></Icon>
+                    <NavText>Configuracoes do app</NavText>
+                </NavItem>
+            </Nav>
+
+            <SignOutButton onPress={() => {}}>
+                <SignOutButtonText> Sair</SignOutButtonText>
+            </SignOutButton>
+        </Container>
     )
 }
--- nubank/src/pages/Main/index.js
+++ nubank/src/pages/Main/index.js
@@ -1,51 +1,100 @@
 import React from 'react';
 
 import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
+  Text, Animated, StyleSheet, Dimensions, ImageBackground, StatusBar,
 } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import Header from '~/components/Header'
 import Tabs from '~/components/Tabs'
 import Menu from '~/components/Menu'
-import { Container,
-         Content,
-         Card,
-         CardHeader,
-         CardContent,
-         Title,
-         Description, 
-         CardFooter, 
-         Annotation } from './styles'
-
-const styles = StyleSheet.create({
-  
-});
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation
+} from './styles'
 
 export default function Main() {
+  let offset = 0
+  const translateY = new Animated.Value(0)
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY
+        }
+      }
+    ],
+    { useNativeDriver: true }
+  )
+
+  function onHandlerStateChange(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false
+      const { translationY } = event.nativeEvent
+      offset += translationY
+
+      if (translationY >= 100) {
+        opened = true
+
+      }else {
+        translateY.setValue(offset)
+        translateY.setOffset(0)
+        offset = 0
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true
+      }).start(() => {
+        offset = opened ? 380 : 0
+        translateY.setOffset(offset)
+        translateY.setValue(0)
+      })
+    }
+  }
+
   return (
     <Container>
       <Header></Header>
       <Content>
-        <Menu></Menu>
-        <Card>
+        <Menu translateY={translateY}></Menu>
+        <PanGestureHandler onGestureEvent={animatedEvent} onHandlerStateChange={onHandlerStateChange}>
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp'
+              })
+            }]
+          }}>
 
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666"></Icon>
-            <Icon name="visibility-off" size={28} color="#666"></Icon>
-          </CardHeader>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666"></Icon>
+              <Icon name="visibility-off" size={28} color="#666"></Icon>
+            </CardHeader>
 
-          <CardContent>
-            <Title> Saldo disponivel</Title>
-            <Description> R$ 197.611,65</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferencia de $S 20,00 recebida de Ana Julia hoje as 18h
-            </Annotation>
-          </CardFooter>
-        </Card>
+            <CardContent>
+              <Title> Saldo disponivel</Title>
+              <Description> R$ 197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferencia de $S 20,00 recebida de Ana Julia hoje as 18h
+               </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
-      <Tabs></Tabs>
+      <Tabs translateY={translateY}></Tabs>
     </Container>
   )
 }
