160c90c7ba513d21555dfb4ba299e28fc5106a0f
jean <jeanlucafp@gmail.com>
2019-10-16 14:09:05 -0300

    trabalho de topicos especiais

---
+++ backend_topico_2/README.md
@@ -0,0 +1,3 @@
+# Trab
+Link para a docs
+https://documenter.getpostman.com/view/7694824/SVtYTnGP?version=latest
---
+++ backend_topico_2/src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+module.exports = mongoose.connect('mongodb://127.0.0.1:27017/mydb', {
+    useNewUrlParser: true
+})
